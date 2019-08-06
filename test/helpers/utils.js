92e60c75b28b4039cc5c3a9baecffd026c63185c
Cristian Necula <virusz@gmail.com>
2019-08-06 15:05:25 +0300

    (tests) rendering while hidden causes legend items to overlap
    Add failing test for known bug
    https://github.com/Neovici/cosmoz-charts/issues/15

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -12,3 +12,30 @@ export const upgradeDomTemplates = () => {
 		};
 	});
 };
+
+const intersects = (el1, el2) => {
+	const r1 = el1.getBoundingClientRect(),
+		r2 = el2.getBoundingClientRect();
+
+	return r2.top >= r1.top && r2.top <= r1.bottom && (r2.left >= r1.left && r2.left <= r1.right)
+		|| r2.bottom >= r1.top && r2.bottom <= r1.bottom && (r2.left >= r1.left && r2.left <= r1.right)
+		|| r2.top >= r1.top && r2.top <= r1.bottom && (r2.right >= r1.left && r2.right <= r1.right)
+		|| r2.bottom >= r1.top && r2.bottom <= r1.bottom && (r2.right >= r1.left && r2.right <= r1.right);
+};
+
+const elementsOverlap = (element, index, elements) =>
+	elements.slice(0, index)
+		.concat(elements.slice(index + 1))
+		.some(otherElement => intersects(element, otherElement) || intersects(otherElement, element));
+
+/** @this chai.Assertion */
+const overlap = function () {
+	const obj = chai.util.flag(this, 'object');
+	this.assert(
+		Array.from(obj).some(elementsOverlap),
+		'expected elements to overlap',
+		'expected elements not to overlap',
+	);
+};
+
+chai.Assertion.addMethod('overlap', overlap);
