c508b617c9330579fa1f2156a9b6ec452084f25e
Meghea Iulian <iulian.meghea@gmail.com>
2022-06-15 07:41:38 +0300

    chore(ci): support beta releases
    Add support for beta releases.

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,16 +1,19 @@
+import cfg from '@neovici/cfg/web/dev-server.mjs';
 import { storybookPlugin } from '@web/dev-server-storybook';
 import { fromRollup } from '@web/dev-server-rollup';
 import rollupReplace from '@rollup/plugin-replace';
 
-const replace = fromRollup(rollupReplace);
+export const replace = fromRollup(rollupReplace)({
+	preventAssignment: true,
+	include: ['node_modules/echarts/**/*.js'],
+	'process.env.NODE_ENV': '"development"',
+});
 
 export default {
+	...cfg,
 	plugins: [
-		replace({
-			preventAssignment: true,
-			include: ['node_modules/echarts/**/*.js'],
-			'process.env.NODE_ENV': '"development"'
-		}),
-		storybookPlugin({ type: 'web-components' })
-	]
+		...cfg.plugins,
+		replace,
+		storybookPlugin({ type: 'web-components' }),
+	],
 };
