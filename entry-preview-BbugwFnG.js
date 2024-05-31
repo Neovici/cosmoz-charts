b02934451a70dfd5d40af5878caa627d7bf5d50c
florianstancioiu <54501671+florianstancioiu@users.noreply.github.com>
2024-05-31 06:10:15 +0000

    Deploying to gh-pages from @ Neovici/cosmoz-charts@c69f7f2f5dd8c57571b2772fcaa1fd9bb66994a3 🚀

---
+++ entry-preview-BbugwFnG.js
@@ -0,0 +1,7 @@
+export { r as render, a as renderToCanvas } from './chunk-FJPRWHXQ-Bj5VwWNo.js';
+import './index-Kjm4kNkQ.js';
+import './lit-html-DUclYDDI.js';
+
+var parameters={renderer:"web-components"};
+
+export { parameters };
